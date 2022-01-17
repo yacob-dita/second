b8fa46b87a00345a33491fb1ffe81c24f745534f
yacob-dita <akodita123@gmail.com>
2022-01-17 09:47:30 -0600

    react files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+const App =()=>{
+  return(
+
+    <div className="container">
+      <h1>Welcome to React!</h1>
+      <h2>Hello React!</h2>
+      <h3>Hello React!</h3>
+    </div>
+    
+  
+  )
+}
+export default App;
